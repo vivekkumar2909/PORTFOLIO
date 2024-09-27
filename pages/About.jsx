ecd5e49d1dec8de352218b1b990e138aa4073e18
vivekkumar2909 <rajvivek620065@gmail.com>
2024-09-28 01:45:07 +0530

    About

--- pages/About.jsx
+++ pages/About.jsx
@@ -2,33 +2,53 @@
 
 export default function About() {
     return (
-        <div className="bg-gray-900 text-white h-full w-full items-center "> 
+        <div className="bg-gray-900 text-white h-full w-full items-center min-h-screen ">
             <div className='m-30 p-10 dark:bg-gray-900 text-center items-center    '>
-                <div>
-                    **Introduction**
-
-                    Hello, I'm Vivek Kumar, a dedicated computer science student with an unwavering enthusiasm for all things related to computers and technology. My journey in the world of computing has been an exciting one, characterized by a constant quest for knowledge and a genuine passion for problem-solving.
+                <div className=" flex flex-col justify-center">
+                    <div className="text-5xl font-bold font-serif">
+                        Introduction
+                    </div>
+                    <div className="justify-center flex">
+                        <div className="w-10/12 justify-center my-5 px-10 text-2xl">
+                            Hello, I'm Vivek Kumar, a dedicated computer science student with an unwavering enthusiasm for all things related to computers and technology. My journey in the world of computing has been an exciting one, characterized by a constant quest for knowledge and a genuine passion for problem-solving.
+                        </div>
+                    </div>
                 </div>
 
                 <div>
-                    <div> Technical Skill</div>
+                    <div className="text-4xl font-bold font-serif"> Technical Skill</div>
+                    <div className="justify-center flex">
+                        <div className="w-10/12 my-6 px-10">
 
-                    At the core of my skill set lies a mastery of programming languages. I am proficient in languages such as C++, C, Python, Java, and JavaScript, which enables me to tackle a wide range of challenges, from low-level system programming to high-level web development.
+                            At the core of my skill set lies a mastery of programming languages. I am proficient in languages such as C++, C, Python, Java, and JavaScript, which enables me to tackle a wide range of challenges, from low-level system programming to high-level web development.
 
-                    In the realm of web development, I have experience with popular front-end technologies like React and Next.js, complemented by my expertise in back-end development using Node.js. My proficiency in database management extends to MySQL, allowing me to design robust and efficient data storage solutions.
+                            In the realm of web development, I have experience with popular front-end technologies like React and Next.js, complemented by my expertise in back-end development using Node.js. My proficiency in database management extends to MySQL, allowing me to design robust and efficient data storage solutions.
+                        </div>
+                    </div>
                 </div>
                 <div>
-                    **Development and Innovation:**
+                    <div className="text-4xl font-bold font-serif"> Work Experience</div>
+                    <div className="justify-center flex">
+                        <div className="w-10/12 my-6 px-10 ">
 
-                    I consider myself a creative problem solver, always eager to push the boundaries of what technology can achieve. Throughout my academic and personal projects, I have actively contributed to the development of innovative solutions. Whether it's designing responsive web applications, optimizing algorithms, or architecting software systems, I thrive in an environment where I can harness technology to create meaningful and impactful solutions.</div>
+                            At the core of my skill set lies a mastery of programming languages. I am proficient in languages such as C++, C, Python, Java, and JavaScript, which enables me to tackle a wide range of challenges, from low-level system programming to high-level web development.
 
-                <div>
-                    **Leadership Qualities:**
-
-                    Beyond my technical prowess, I possess strong leadership qualities. I believe in the power of collaboration and effective teamwork to achieve goals. Throughout my academic journey, I have taken on leadership roles in group projects, guiding teams toward successful outcomes by fostering a sense of unity and shared purpose. My ability to communicate ideas, mentor peers, and take initiative has been instrumental in leading teams to success.
-
-                    In summary, I am a computer science enthusiast with a broad skill set and a passion for development, armed with the technical expertise and leadership qualities needed to make a significant impact in the world of technology. I am excited to continue my journey, constantly learning and innovating, and contributing positively to the ever-evolving field of computer science.
+                            In the realm of web development, I have experience with popular front-end technologies like React and Next.js, complemented by my expertise in back-end development using Node.js. My proficiency in database management extends to MySQL, allowing me to design robust and efficient data storage solutions.
+                        </div>
+                    </div>
                 </div>
+                <div>
+                    <div className="text-4xl font-bold font-serif">Cover Letter</div>
+                    <div className="justify-center flex">
+                        <div className="w-10/12 my-6 px-10 flex ">
+
+                            At the core of my skill set lies a mastery of programming languages. I am proficient in languages such as C++, C, Python, Java, and JavaScript, which enables me to tackle a wide range of challenges, from low-level system programming to high-level web development.
+
+                            In the realm of web development, I have experience with popular front-end technologies like React and Next.js, complemented by my expertise in back-end development using Node.js. My proficiency in database management extends to MySQL, allowing me to design robust and efficient data storage solutions.
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
